95cc033c1c1593e66d85074fdcf5adb6fab67509
David <566956+davidallenr@users.noreply.github.com>
2024-02-14 18:37:38 -0500

    Adding new ignore, refactored code, new requirements updated

--- main.py
+++ main.py
@@ -1,103 +1,153 @@
-import requests
+import hashlib
+import json
 import os
-import concurrent.futures
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+import requests
 from tqdm import tqdm
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from datetime import datetime
-from time import sleep
 from decouple import config
+import hashlib
+import concurrent.futures
 
 
-# These values are stored in the .env that you must create
-WEBDRIVER_PATH = config("WEBDRIVER_PATH")
+# Configuration and Setup
 LOGIN_URL = config("LOGIN_URL")
 USERNAME = config("ACCOUNT_USERNAME")
 PASSWORD = config("PASSWORD")
-WAIT_TIME = config("WAIT_TIME", default=0)
-CHILDS_NAME = config("CHILDS_NAME")
-HEADLESS = config("HEADLESS", default=False)
-CORE_COUNT = config("CORE_COUNT", default = 1)  ## Set this to the physical core count on your cpu
-MAX_WORKER = 2 * int(CORE_COUNT) + 1
-LAST_DATE_FILENAME = "last_date.txt"
-
-def retrieve_media_from_container_of_links(container_of_links_and_path):
-    href = container_of_links_and_path['href']
-    path = container_of_links_and_path['path']
-
-    # If that file does NOT exist then retrieve
-    try:
-        if not os.path.exists(path):
-            response = requests.get(href)
-            with open(path, 'wb') as f:
-                f.write(response.content)
-    except requests.exceptions.HTTPError:
-        print("Unable to retrieve file:" + path)
-
-def concurrently_retrieve_media_from_container_of_links(container_of_links_and_path):
-    with tqdm(total=len(container_of_links_and_path), desc='Download Progress') as pbar:
-        with concurrent.futures.ProcessPoolExecutor(max_workers=MAX_WORKER) as executor:
-            for _ in executor.map(retrieve_media_from_container_of_links, container_of_links_and_path, timeout=60):
-                pbar.update()
+CHILD_NAME = config("CHILD_NAME")
+WEBDRIVER_PATH = config("WEBDRIVER_PATH")
+HEADLESS = config("HEADLESS", default="False").lower() in ["true", "1", "t", "y", "yes"]
+DOWNLOAD_DIR = "downloads"
+HASH_RECORDS = "downloaded_files.json"
+MAX_WORKERS = config("MAX_WORKERS", default=4, cast=int)
 
 
-# Read the last date from the file
-def read_last_date():
-    if os.path.exists(LAST_DATE_FILENAME):
-        with open(LAST_DATE_FILENAME, "r") as f:
-            return f.read()
-    return None
+# Ensure the download directory and hash records file exist
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+if not os.path.exists(HASH_RECORDS):
+    with open(HASH_RECORDS, "w") as f:
+        json.dump({}, f)
 
-# Write the last date to the file
-def write_last_date(last_date):
-    with open(LAST_DATE_FILENAME, "w") as f:
-        f.write(last_date)
 
-def add_links_and_path_to_containers(list_of_links, driver, image_container, video_container):
-    item_count = 1
-    last_date = read_last_date()
-    count = 0
-    for link in list_of_links:
-        # Get the date from the TD element related to the object
-        webdate = driver.find_element(
-            By.XPATH, "//*/table/tbody/tr[" + str(item_count) + "]/td[2]"
-        ).text.replace("/", "-")
-    
-        date_obj = datetime.strptime(webdate, '%m-%d-%y')   
-        date = str(date_obj.date())
-    
-        # Increment the count if the date is the same as the last item
-        if date == last_date:
-            count += 1
-        else:
-            count = 0
-            last_date = date
-            write_last_date(last_date)
+def load_downloaded_hashes():
+    with open(HASH_RECORDS, "r") as f:
+        return json.load(f)
 
-        if link.get_attribute("title") == "Download Image":
-            image_container.append({
-                "href": link.get_attribute("href"),
-                "path": "img/" + date + "_" + CHILDS_NAME + "_" + str(count) + ".jpg"
-            })
 
-        if link.get_attribute("title") == "Download Video":
-            video_container.append({
-                "href": link.get_attribute("href"),
-                "path": "mov/" + date + "_" + CHILDS_NAME + "_" + str(count) + ".mov"
-            })
+def save_downloaded_hash(file_name, file_hash):
+    hashes = load_downloaded_hashes()
+    hashes[file_name] = file_hash
+    with open(HASH_RECORDS, "w") as f:
+        json.dump(hashes, f, indent=4)
 
-        item_count += 1
+
+def setup_driver():
+    chrome_options = webdriver.ChromeOptions()
+    if HEADLESS:
+        chrome_options.add_argument("--headless")
+
+    # Suppress DevTools logs
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+    # Add this to suppress console errors
+    chrome_options.add_argument("--log-level=3")
+
+    service = Service(WEBDRIVER_PATH)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    return driver
+
 
 def login(driver):
-    elem_user_login = driver.find_element(By.NAME, "user[login]")
-    elem_user_password = driver.find_element(By.NAME, "user[password]")
-    elem_user_login.clear()
-    elem_user_login.send_keys(USERNAME)
-    elem_user_password.clear()
-    elem_user_password.send_keys(PASSWORD)
-    elem_user_password.send_keys(Keys.RETURN)
+    print("Attempting to log in...")
+    driver.get(LOGIN_URL)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "user[login]"))
+    )
+    driver.find_element(By.NAME, "user[login]").send_keys(USERNAME)
+    print("Sending username")
+    driver.find_element(By.NAME, "user[password]").send_keys(PASSWORD)
+    print("Sending password")
+    driver.find_element(By.NAME, "user[password]").submit()
+
+    # Adjust the below line to wait for an element that indicates successful login
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "contacts-close-button"))
+        ).click()
+        print("Logged in, Popup closed.")
+    except Exception as e:
+        print(f"No popup to close or error closing popup: {e}")
+
+    # Navigate to Entries
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.LINK_TEXT, "Entries"))
+        ).click()
+        print("Navigated to Entries page.")
+    except TimeoutException:
+        print("Failed to navigate to Entries page.")
+
+
+def get_media_links_and_dates(driver):
+    media_links_and_dates = []
+    elements = driver.find_elements(
+        By.XPATH, "//*[@title='Download Image' or @title='Download Video']"
+    )
+    dates = driver.find_elements(By.XPATH, "//*/table/tbody/tr/td[2]")
+
+    for i, element in enumerate(elements):
+        date_text = dates[i].text.strip()
+        try:
+            date_object = datetime.strptime(date_text, "%m/%d/%y")
+            formatted_date = date_object.strftime("%Y-%m-%d")
+        except ValueError:
+            formatted_date = datetime.now().strftime("%Y-%m-%d")
+
+        # Determine file extension based on the element's title attribute
+        title = element.get_attribute("title")
+        if "Download Image" in title:
+            file_extension = "jpg"
+        elif "Download Video" in title:
+            file_extension = "mp4"
+        else:
+            file_extension = "unknown"
+
+        media_links_and_dates.append(
+            {
+                "url": element.get_attribute("href"),
+                "date": formatted_date,
+                "extension": file_extension,  # Include the determined file extension in the dict
+            }
+        )
+
+    return media_links_and_dates
+
+
+def download_media(media_info):
+    url = media_info["url"]
+    date = media_info["date"]
+    file_extension = media_info["extension"]
+
+    try:
+        response = requests.get(url, timeout=30)
+        content_hash = hashlib.md5(response.content).hexdigest()
+        file_name = f"{date}_{CHILD_NAME}_{content_hash[:8]}.{file_extension}"
+        file_path = os.path.join(DOWNLOAD_DIR, file_name)
+
+        downloaded_hashes = load_downloaded_hashes()
+        # Check if the file has already been downloaded by comparing hashes
+        if content_hash not in downloaded_hashes.values():
+            with open(file_path, "wb") as file:
+                file.write(response.content)
+            save_downloaded_hash(file_name, content_hash)
+    except Exception as e:
+        print(f"Failed to download {url}: {e}")
+
 
 def return_last_page(driver):
     div = driver.find_element(By.XPATH, "//*[@class='pagination']/li[last()]/a")
@@ -106,66 +156,62 @@ def return_last_page(driver):
 
     return int(last_page)
 
+
+def concurrently_retrieve_media_from_container_of_links(all_media_info):
+    with tqdm(total=len(all_media_info), desc="Download Progress") as pbar:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+            # Submit all download tasks and create a list of futures
+            futures = [executor.submit(download_media, info) for info in all_media_info]
+            # Wait for the futures to complete and update the progress bar as each is done
+            for future in concurrent.futures.as_completed(futures):
+                # Each time a future completes, update the progress bar
+                pbar.update(1)
+
+
 def main():
-    chrome_options = Options()
+    print("Script started.")
+    driver = setup_driver()
+    try:
+        login(driver)
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//*/table/tbody/tr/td[2]"))
+        )
+        print("Successfully logged in and on the correct page.")
 
-    if HEADLESS:
-        chrome_options.add_argument("--headless")
+        all_media_info = []
+        last_page_reached = False
 
-    driver = webdriver.Chrome(WEBDRIVER_PATH, options=chrome_options)
-    driver.implicitly_wait(WAIT_TIME)
+        # Assuming you have a way to determine the total number of pages upfront
+        total_pages = return_last_page(driver)
+        print(f"Total pages to process: {total_pages}")
 
-    driver.get(LOGIN_URL)
+        for _ in tqdm(range(1, total_pages + 1), desc="Navigating Pages"):
+            if last_page_reached:
+                break
+            all_media_info.extend(get_media_links_and_dates(driver))
+            try:
+                next_button = WebDriverWait(driver, 5).until(
+                    EC.element_to_be_clickable(
+                        (By.XPATH, "//*[@class='pagination']/li/a[@rel='next']")
+                    )
+                )
+                next_button.click()
+            except TimeoutException:
+                print("No more pages to scrape or next page button not found.")
+                last_page_reached = True
 
-    assert "KinderCare" in driver.title
+        # Ensure there's something to download
+        if all_media_info:
+            print(f"Starting download of {len(all_media_info)} items...")
+            concurrently_retrieve_media_from_container_of_links(all_media_info)
+        else:
+            print("No media information gathered to download.")
 
-    # Find the username and password elements
-    # Fill out username and password then send the return key
-    # LOGIN SECTION
-    print("Logging into kindercare")
-    login(driver)
-
-    # wait for the page to load
-    sleep(.25)
-
-    # Close popup window
-    driver.find_element(By.CLASS_NAME, "contacts-close-button").click()
-
-    # Navigate to Entries
-    sleep(.25)
-    print("Navigating to correct page to begin link scraping")
-    driver.find_element(By.LINK_TEXT, "Entries").click()
-
-    current_page = 1
-    last_page = return_last_page(driver)
-
-    image_container = []
-    video_container = []
-
-    for current_page in tqdm(range(1, last_page), desc='Scraping Progress'):
-        # Code for scraping the links from the website goes here
-        try:
-            next_found_on_page = driver.find_element(By.XPATH, "//*[@class='pagination']/li/a[@rel='next']") 
-            if next_found_on_page is not None:
-                next_found_on_page.click()
-                # print("Scraping links from page: " + str(current_page) + " of " + str(last_page) + "Progress: " + str(current_page/last_page * 100) + "%")
-                # Store all image and video links
-                list_of_images = driver.find_elements(By.XPATH, "//*[@title='Download Image' or @title='Download Video']")
-                
-                add_links_and_path_to_containers(list_of_images, driver, image_container, video_container)
-
-                current_page += 1
-        except:
-            print("Unable to find next button on page: " + str(current_page))
-            break
-
-    print("Beginning video retrieval")
-    concurrently_retrieve_media_from_container_of_links(video_container)
-    print("Beginning image retrieval")
-    concurrently_retrieve_media_from_container_of_links(image_container)
-    
-    sleep(1)
-    driver.quit()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        driver.quit()
+        print("Script finished.")
 
 
 if __name__ == "__main__":
